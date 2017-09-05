1865fabb3ca86438b877d04a59d8d44d712c7a55
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-09-05 11:29:20 +1000

    licence info

--- src/main/java/com/declarativesystems/pejava/codemanager/Deploy.java
+++ src/main/java/com/declarativesystems/pejava/codemanager/Deploy.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2017 Geoff Williams for Declarative Systems PTY LTD
+ * Copyright 2016 Geoff Williams for Puppet Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.declarativesystems.pejava.codemanager;
 
 import com.google.gson.Gson;
