03f575eca4d0aa36ea51c7fe226262f238a57e5c
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-09-10 15:54:23 +1000

    support for mock cacerts

--- src/test/java/test/com/declarativesystems/codemanager/TestDeploy.java
+++ src/test/java/test/com/declarativesystems/codemanager/TestDeploy.java
@@ -2,10 +2,12 @@ package test.com.declarativesystems.codemanager;
 
 import com.declarativesystems.pejava.codemanager.Deploy;
 import com.declarativesystems.pejava.codemanager.DeployImpl;
+import org.apache.commons.io.IOUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -18,8 +20,9 @@ public class TestDeploy {
     private Deploy deploy = new DeployImpl();
     private final static String PUPPET_DEPLOY_OK = "PUPPET_DEPLOY_OK";
     private static String PUPPET_MASTER_FQDN = "not ready yet";
-    //private final static String CA_CERT_FILE =
     private final static String[] ENVIRONMENT = {"production"};
+    private final static String CA_CERT_FILE = "/tmp/mock_code_manager_ssl/ca.crt";
+    private static String CA_CERT;
     private static Process mockCodeManager;
 
     @BeforeClass
@@ -27,13 +30,14 @@ public class TestDeploy {
         try {
             PUPPET_MASTER_FQDN = InetAddress.getLocalHost().getHostName();
             mockCodeManager = Runtime.getRuntime().exec("mock_code_manager");
-
             // wait for server to become ready
             try {
                 Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            // CA cert is generated in /tmp by the above command
+            CA_CERT = IOUtils.toString(new FileInputStream(CA_CERT_FILE));
         } catch (IOException e) {
             // JRuby was too hard so just shell out and run the mock_code_manager rubygem
             System.err.println("************************************************************");
@@ -55,18 +59,26 @@ public class TestDeploy {
 
     @Test
     public void testOkWithCaCert() throws Exception {
-        deploy.deployCode(PUPPET_MASTER_FQDN, PUPPET_DEPLOY_OK, null, ENVIRONMENT);
+        String ret = deploy.deployCode(PUPPET_MASTER_FQDN, PUPPET_DEPLOY_OK, CA_CERT, ENVIRONMENT);
+        assertTrue(ret.contains("queued"));
     }
 
     @Test
     public void testOkWithoutCaCert() throws Exception {
-        deploy.deployCode(PUPPET_MASTER_FQDN, PUPPET_DEPLOY_OK, null, ENVIRONMENT);
+        String ret = deploy.deployCode(PUPPET_MASTER_FQDN, PUPPET_DEPLOY_OK, null, ENVIRONMENT);
+        assertTrue(ret.contains("queued"));
+    }
+
+    @Test(expected=javax.net.ssl.SSLHandshakeException.class)
+    public void testWithWrongCaCert() throws Exception {
+        String cert = IOUtils.toString(this.getClass().getResourceAsStream("/ca.pem"));
+        deploy.deployCode(PUPPET_MASTER_FQDN, PUPPET_DEPLOY_OK, cert, ENVIRONMENT);
     }
 
 
     @Test
     public void testBadTokenWithCaCert() throws Exception {
-        String ret = deploy.deployCode(PUPPET_MASTER_FQDN, "BAD TOKEN", null, ENVIRONMENT);
+        String ret = deploy.deployCode(PUPPET_MASTER_FQDN, "BAD TOKEN", CA_CERT, ENVIRONMENT);
         assertTrue(ret.contains("kind"));
     }
 
